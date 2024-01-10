af118a06460385c64c3d88ff6b1ff73c4a4aef1a
NityaBaghel <149133153+NityaBaghel@users.noreply.github.com>
2024-01-10 14:18:33 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,8 @@
+// javascript is a losely typed programming language means it doesn't attaches any datatype to its variable
+
+var a;
+console.log(a);
+ a=10;
+console.log(a);
+a="Hello World";
+console.log(a);
